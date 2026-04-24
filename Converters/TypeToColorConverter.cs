28a0b4abb1bc6730a15f2971d2ff72a6c42ae6e0
Полина Смирнова <pnsmirno@gmail.com>
2026-04-24 23:01:22 +0300

    Красит приход в зелёный, расход в красный

---
+++ Converters/TypeToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using WarehouseApp.Models;
+
+namespace WarehouseApp.Converters
+{
+    public class TypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MovementType type)
+            {
+                return type == MovementType.In ? Brushes.Green : Brushes.Red;
+            }
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
