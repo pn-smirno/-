be5c972a1f7e498e13898717661efee583215345
Полина Смирнова <pnsmirno@gmail.com>
2026-04-24 22:58:27 +0300

    Код, который создаёт базу данных при запуске

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WarehouseApp.Data;
+
+namespace WarehouseApp
+{
+    public partial class App : Application  // ← ДОЛЖНО БЫТЬ "partial"
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            using (var db = new AppDbContext())
+            {
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+}
